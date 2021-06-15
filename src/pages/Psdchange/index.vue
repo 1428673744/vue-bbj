ccd131175fd251af81fe42101437e315f84f5f26
1428673744 <1428673744@qq.com>
2021-06-15 19:10:34 +0800

    c

--- src/pages/Psdchange/index.vue
+++ src/pages/Psdchange/index.vue
@@ -51,9 +51,7 @@ export default {
                 headers: {
                     'Content-Type': 'application/json;charset=utf-8'     
                 },
-                data:{
-                  
-                }              
+                data: JSON.stringify(_this.pwd),                           
             }).then (function (response) {
                 eventBus.$emit("sisterSaid",response.data);
             }).catch (function (error) {
